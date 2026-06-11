428352323bae30ecb2091fe77316e6ad1c86da9f
Justin van der Mey <justinvdmey@hotmail.com>
2026-06-11 19:18:50 +0200

    fix: Resolved terraform formatting issue

--- terraform/hybrid/main.tf
+++ terraform/hybrid/main.tf
@@ -194,16 +194,16 @@ locals {
 resource "local_file" "terraform_inventory" {
   filename = "${path.module}/inventory.ini"
   content = templatefile("${path.module}/templates/inventory.tpl", {
-    azure_vm_name         = var.azure_vm_name
-    azure_public_ip       = local.azure_public_ip
-    esxi_vm_name          = var.esxi_vm_name
-    esxi_host_ip          = local.esxi_host_ip
-    ansible_user          = var.ansible_user
-    ssh_private_key_path  = var.ssh_private_key_path
-    testuser_private_key  = local_sensitive_file.testuser_private_key.filename
-    testuser_public_key   = local_file.testuser_public_key.filename
-    container_image       = var.container_image
-    container_name        = var.container_name
+    azure_vm_name        = var.azure_vm_name
+    azure_public_ip      = local.azure_public_ip
+    esxi_vm_name         = var.esxi_vm_name
+    esxi_host_ip         = local.esxi_host_ip
+    ansible_user         = var.ansible_user
+    ssh_private_key_path = var.ssh_private_key_path
+    testuser_private_key = local_sensitive_file.testuser_private_key.filename
+    testuser_public_key  = local_file.testuser_public_key.filename
+    container_image      = var.container_image
+    container_name       = var.container_name
   })
 }
 
