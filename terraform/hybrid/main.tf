6f5e14e8a8d1dbe8540b9b8ae4cf82a5c0ae3120
Justin van der Mey <justinvdmey@hotmail.com>
2026-06-11 19:44:14 +0200

    Fix terraform fmt check

--- terraform/hybrid/main.tf
+++ terraform/hybrid/main.tf
@@ -130,17 +130,17 @@ resource "azurerm_network_interface_security_group_association" "hybrid" {
 }
 
 resource "azurerm_linux_virtual_machine" "hybrid" {
-  name                  = var.azure_vm_name
-  resource_group_name   = azurerm_resource_group.hybrid.name
-  location              = azurerm_resource_group.hybrid.location
-  size                  = var.azure_vm_size
-  admin_username        = var.ansible_user
+  name                = var.azure_vm_name
+  resource_group_name = azurerm_resource_group.hybrid.name
+  location            = azurerm_resource_group.hybrid.location
+  size                = var.azure_vm_size
+  admin_username      = var.ansible_user
   network_interface_ids = [
     azurerm_network_interface.hybrid.id
   ]
 
   disable_password_authentication = true
-  custom_data                     = base64encode(templatefile("${path.module}/templates/cloud-init-user-data.tpl", {
+  custom_data = base64encode(templatefile("${path.module}/templates/cloud-init-user-data.tpl", {
     ansible_user   = var.ansible_user
     ssh_public_key = chomp(file(pathexpand(var.ssh_public_key_path)))
   }))
@@ -178,7 +178,7 @@ resource "esxi_guest" "hybrid" {
   }
 
   guestinfo = {
-    "userdata"          = base64gzip(templatefile("${path.module}/templates/cloud-init-user-data.tpl", {
+    "userdata" = base64gzip(templatefile("${path.module}/templates/cloud-init-user-data.tpl", {
       ansible_user   = var.ansible_user
       ssh_public_key = chomp(file(pathexpand(var.ssh_public_key_path)))
     }))
@@ -209,5 +209,5 @@ resource "local_file" "terraform_inventory" {
 
 resource "local_file" "ansible_inventory" {
   filename = "${path.module}/../../ansible/inventory.ini"
-  content = local_file.terraform_inventory.content
+  content  = local_file.terraform_inventory.content
 }
