d53dea7f02fa62574abe8586790621ee5fd63a32
Justin van der Mey <justinvdmey@hotmail.com>
2026-06-11 19:22:19 +0200

    fix: formatting

--- terraform/hybrid/main.tf
+++ terraform/hybrid/main.tf
@@ -130,11 +130,11 @@ resource "azurerm_network_interface_security_group_association" "hybrid" {
 }
 
 resource "azurerm_linux_virtual_machine" "hybrid" {
-  name                = var.azure_vm_name
-  resource_group_name = azurerm_resource_group.hybrid.name
-  location            = azurerm_resource_group.hybrid.location
-  size                = var.azure_vm_size
-  admin_username      = var.ansible_user
+  name                  = var.azure_vm_name
+  resource_group_name   = azurerm_resource_group.hybrid.name
+  location              = azurerm_resource_group.hybrid.location
+  size                  = var.azure_vm_size
+  admin_username        = var.ansible_user
   network_interface_ids = [
     azurerm_network_interface.hybrid.id
   ]
@@ -178,7 +178,7 @@ resource "esxi_guest" "hybrid" {
   }
 
   guestinfo = {
-    "userdata" = base64gzip(templatefile("${path.module}/templates/cloud-init-user-data.tpl", {
+    "userdata"          = base64gzip(templatefile("${path.module}/templates/cloud-init-user-data.tpl", {
       ansible_user   = var.ansible_user
       ssh_public_key = chomp(file(pathexpand(var.ssh_public_key_path)))
     }))
